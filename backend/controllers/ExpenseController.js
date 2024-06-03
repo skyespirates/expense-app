bd8a56440aa18cad33faa6a3248c3435f46568cd
Aziz <akashiseijurou414@gmail.com>
2024-06-03 07:34:49 +0700

    add expense backend functionality
    # add controller to every expense route
    # add model corelated to its controller
    # modify table expense
    # add table user
    # create one to many relationship between table user and expense

--- backend/controllers/ExpenseController.js
+++ backend/controllers/ExpenseController.js
@@ -1,9 +1,8 @@
+const ExpenseModel = require('../models/ExpenseModel')
 
 const getAllExpenses = async (request, reply) => {
       try {
-            const connection = await request.getConnection();
-            const [rows, fields] = await connection.query('SELECT * FROM student');
-            connection.release();
+            const [rows, fields] = await ExpenseModel.getAllExpense(1)
             return reply.response(rows);
       } catch (error) {
             return reply.response("Something went wrong!")
@@ -12,43 +11,39 @@ const getAllExpenses = async (request, reply) => {
 
 const getExpenseById = async (request, reply) => {
       try {
-            const response = {
-                  message: "getExpenseById"
-            }
-            return reply.response(response)
+            const [rows, fields] = await ExpenseModel.getExpenseById({expenseId: 3, userId: 2})
+            return reply.response(rows)
       } catch (error) {
             return reply.response("Something went wrong!")
       }
 }
 
 const addExpense = async (request, reply) => {
+      const {title, amount} = request.payload;
       try {
-            const response = {
-                  message: "addExpense"
-            }
-            return reply.response(response)
+            const [rows, _] = await ExpenseModel.addExpense({title, amount, user_id: 1})
+            return reply.response(rows)
       } catch (error) {
             return reply.response("Something went wrong!")
       }
 }
 
 const updateExpense = async (request, reply) => {
+      const {expenseId} = request.params;
+      const {title, amount} = request.payload;
       try {
-            const response = {
-                  message: "updateExpense"
-            }
-            return reply.response(response)
+            const [rows, _] = await ExpenseModel.updateExpense({expenseId, title, amount})
+            return reply.response(rows)
       } catch (error) {
             return reply.response("Something went wrong!")
       }
 }
 
 const deleteExpense = async (request, reply) => {
+      const {expenseId} = request.params;
       try {
-            const response = {
-                  message: "deleteExpense"
-            }
-            return reply.response(response)
+            const [rows, _]  = await ExpenseModel.deleteExpense(expenseId)
+            return reply.response(rows)
       } catch (error) {
             return reply.response("Something went wrong!")
       }
